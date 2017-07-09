9dcbb8848c362a909973d71bf519487c9ab27a1f
Gabe Levasseur <gabriel.levasseur@sagaprograms.com>
2017-07-08 19:12:36 -0700

    Added watson factory

---
+++ app/factories/watson.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = Watson;
+
+
+/* @ngInject */
+function Watson($http) {
+    return {
+        search
+    };
+
+    function search(query) {
+        // eslint-disable-next-line
+        let url = `/watson`;
+
+        return $http.post(url, {query })
+    }
+}
+
