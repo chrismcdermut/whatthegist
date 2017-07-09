8ade876c0b8ac36cf822030426ac08f1c520489d
parakhkunal <parakh.kunal@gmail.com>
2017-07-09 01:55:07 -0700

    watson working

--- app/home/home-controller.js
+++ app/home/home-controller.js
@@ -1,4 +1,20 @@
-function homeController() {
+/* @ngInject */
+function homeController($log, watsonFactory, $scope) {
+    //Pass the search query here and return top 3 result documents
+    watsonFactory.search("did batman ever die")
+        .then(function(response) {
+            $scope.rankedResult = [];
+            $scope.isNoResults = false;
+            if(response.status === 200 && response.data.numFound !== 0) {
+                let resultSet = response.data.docs;
+                for(let i = 0; i < 3; i++) {
+                    $scope.rankedResult[i] = resultSet[i].title[0] || '';
+                }
+            } else {
+                $scope.isNoResults = true;
+            }
+        })
+        .catch($log.log)
 }
 
 export default homeController;
--- server/watson.js
+++ server/watson.js
@@ -6,13 +6,13 @@ module.exports = handle;
 
 console.log('Starting gistice function');
 function handle({query}, ctx, cb) {
-    let url = `https://107e971a-6955-42da-b030-d7f553269849:QmiwQ5Py3dG7@gateway.watsonplatform.net/retrieve-and-rank/api/v1/solr_clusters/sc145e7ad4_a313_44dc_abb7_8272076ca6d3/solr/sh_collection/select?q=${query}z&wt=json&fl=id,title`;
+    let url = `https://107e971a-6955-42da-b030-d7f553269849:QmiwQ5Py3dG7@gateway.watsonplatform.net/retrieve-and-rank/api/v1/solr_clusters/sc145e7ad4_a313_44dc_abb7_8272076ca6d3/solr/gisticeleague_final_collection/select?q=how many times did batman die&wt=json&fl=*&debug=results`;
 
     request(url, (err, status, res) => {
         err && cb(err);
         try {
             res = JSON.parse(res);
-            cb(null, res.response.docs);
+            cb(null, res.response);
         } catch(e) {
             cb(new Error('Could not parse json'))
 
